aa0013b09e2dabd8ebdeffcf6193816f698f9130
ulirodriguezz <ulirodrigueze@gmail.com>
2025-07-07 22:43:32 -0300

    feat: add Item Mapper methods

--- src/main/java/com/example/demo/mapper/ItemMapper.java
+++ src/main/java/com/example/demo/mapper/ItemMapper.java
@@ -1,8 +1,30 @@
 package com.example.demo.mapper;
 
+import com.example.demo.dto.SimpleItemDTO;
+import com.example.demo.model.Item;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ItemMapper {
+
+    public Item toItemEntity(SimpleItemDTO dto){
+        Item item = new Item();
+        item.setDescription(dto.getDescription());
+        item.setName(dto.getName());
+        item.setTagId(dto.getTagId());
+        item.setLocationDescription(dto.getLocationDescription());
+        return item;
+    }
+
+    public SimpleItemDTO toSimpleItemDTO(Item item){
+        SimpleItemDTO dto = new SimpleItemDTO.Builder()
+                .id(item.getId())
+                .name(item.getName())
+                .description(item.getDescription())
+                .locationDescription(item.getLocationDescription())
+                .tagId(item.getTagId())
+                .build();
+        return dto;
+    }
 
 }
