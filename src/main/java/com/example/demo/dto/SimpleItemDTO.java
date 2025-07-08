7263d816afe608733c87276e068ae6be8672824f
ulirodriguezz <ulirodrigueze@gmail.com>
2025-07-07 22:40:00 -0300

    feat: add Item Mapper and Simple DTO

---
+++ src/main/java/com/example/demo/dto/SimpleItemDTO.java
@@ -0,0 +1,100 @@
+package com.example.demo.dto;
+public class SimpleItemDTO {
+
+    private Long id;
+
+    private String tagId;
+
+    private String name;
+
+    private String description;
+
+    private String locationDescription;
+
+    public SimpleItemDTO(Builder builder){
+        this.id = builder.id;
+        this.tagId = builder.tagId;
+        this.name = builder.name;
+        this.description = builder.description;
+        this.locationDescription = builder.locationDescription;
+    }
+
+    public static class Builder{
+        private Long id;
+
+        private String tagId;
+
+        private String name;
+
+        private String description;
+
+        private String locationDescription;
+        public Builder id(Long id){
+            this.id = id;
+            return this;
+        }
+        public Builder tagId (String tagId){
+            this.tagId = tagId;
+            return this;
+        }
+        public Builder name (String name){
+            this.name = name;
+            return this;
+        }
+        public Builder description(String description){
+            this.description = description;
+            return this;
+        }
+        public Builder locationDescription(String locationDescription){
+            this.locationDescription = locationDescription;
+            return this;
+        }
+        public SimpleItemDTO build(){
+           return new SimpleItemDTO(this);
+        }
+
+    }
+
+    public SimpleItemDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(String tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocationDescription() {
+        return locationDescription;
+    }
+
+    public void setLocationDescription(String locationDescription) {
+        this.locationDescription = locationDescription;
+    }
+}
