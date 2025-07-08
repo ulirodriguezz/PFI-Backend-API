bf07c9213655d37468fb1d98020f68a0f5f1247f
ulirodriguezz <ulirodrigueze@gmail.com>
2025-07-07 22:18:11 -0300

    feat: add Item entity

---
+++ src/main/java/com/example/demo/model/Item.java
@@ -0,0 +1,68 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "items")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "tag_id",nullable = true)
+    private String tagId;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "description",nullable = false,length = 200)
+    private String description;
+
+    @Column(name = "location_description",nullable = true,length = 255)
+    private String locationDescription;
+    // Falta la parte de las fotos
+
+
+    public Item() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(String tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocationDescription() {
+        return locationDescription;
+    }
+
+    public void setLocationDescription(String locationDescription) {
+        this.locationDescription = locationDescription;
+    }
+}
