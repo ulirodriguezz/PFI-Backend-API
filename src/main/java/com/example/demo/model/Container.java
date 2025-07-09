026a8911162a49930524f64f13796675ae78168a
ulirodriguezz <ulirodrigueze@gmail.com>
2025-07-09 18:13:52 -0300

    feat: add Container Entity

---
+++ src/main/java/com/example/demo/model/Container.java
@@ -0,0 +1,56 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "containers")
+public class Container {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name",nullable = false)
+    private String name;
+    @Column(name = "description",nullable = false)
+    private String description;
+    @OneToMany(mappedBy = "container",cascade = CascadeType.ALL)
+    private List<Item> items;
+    //Falta la foto del contenedor
+
+
+    public Container() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
