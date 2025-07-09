0cf688796168666763dba04926c0214e3b24b783
ulirodriguezz <ulirodrigueze@gmail.com>
2025-07-09 18:16:33 -0300

    feat: add Container Repository

---
+++ src/main/java/com/example/demo/repository/ContainerRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Container;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Repository
+public interface ContainerRepository extends JpaRepository<Container,Long> {
+    Optional<Container> getContainerById(long id);
+    Set<Container> getAllByNameContaining(String name);
+
+}
